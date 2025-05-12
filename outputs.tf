6130bc78f79b8f1cb1e1b5e0d121db7ed4a7d817
Thomas De Meyer <t.demeyer@labdigital.nl>
2025-05-12 15:17:45 +0200

    feat: add principal_id to output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "id" {
   value       = azurerm_linux_function_app.this.id
 }
 
+output "principal_id" {
+  description = "The principal ID of the function app"
+  value       = azurerm_linux_function_app.this.identity[0].principal_id
+}
+
 output "default_hostname" {
   description = "The default hostname of the function app"
   value       = azurerm_linux_function_app.this.default_hostname
